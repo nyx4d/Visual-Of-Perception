0291b171fb75ed0ff6b6a36d05b5e9d7c2a9f9e1
nyx4d <nyx4d@proton.me>
2024-10-05 00:01:08 -0400

    Update helpers.js

--- js/helpers.js
+++ js/helpers.js
@@ -1,14 +1,12 @@
+// Update mouse coordinates in the DOM, including the Z coordinate
 function updateMouseCoords() {
     var mouseCoordsElement = document.getElementById('mouseCoords');
-    var xPixels = ((mouse.x + 1) / 2 * window.innerWidth).toFixed(2);
-    var yPixels = ((-mouse.y + 1) / 2 * window.innerHeight).toFixed(2);
-    mouseCoordsElement.innerHTML = `X: ${xPixels}px, Y: ${yPixels}px`;
+    mouseCoordsElement.innerHTML = `X: ${intersectionPoint.x.toFixed(2)}, Y: ${intersectionPoint.y.toFixed(2)}, Z: ${intersectionPoint.z.toFixed(2)}`;
 }
 
-
+// Update the scene volume in the DOM
 function updateSceneVolume() {
     var sceneVolumeElement = document.getElementById('sceneVolume');
-    var cameraZ = camera.position.z.toFixed(2);
-    var volume = (window.innerWidth * window.innerHeight * cameraZ).toFixed(2);
-    sceneVolumeElement.innerHTML = `${volume} px³ (Camera Z: ${cameraZ})`;
+    var volume = (window.innerWidth * window.innerHeight * camera.position.z).toFixed(2);
+    sceneVolumeElement.innerHTML = `${volume} px³`;
 }
