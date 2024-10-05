5f3807eb1391aa6c9682e7128a82390d5815794a
nyx4d <nyx4d@proton.me>
2024-10-05 00:02:30 -0400

    Update interaction.js

--- js/interaction.js
+++ js/interaction.js
@@ -1,10 +1,20 @@
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
+var intersectionPoint = new THREE.Vector3();  // For storing the intersection point
+
+// Plane to intersect with for mouse position (a plane at Z=0)
+var plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
 
 // Mouse coordinates display
 window.addEventListener('mousemove', function(event) {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    // Update Z-coordinate by raycasting to a plane
+    raycaster.setFromCamera(mouse, camera);
+    raycaster.ray.intersectPlane(plane, intersectionPoint);
+
+    updateMouseCoords();  // Update X, Y, Z in the DOM
 });
 
 // Node interaction: clicking on a node
@@ -14,17 +24,6 @@ window.addEventListener('click', function(event) {
 
     if (intersects.length > 0) {
         var intersectedNode = intersects[0].object;
-        intersectedNode.material.color.set(0xff0000); // Change color on click
-    }
-});
-
-// Keyboard interaction for switching layouts
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'g') {
-        clearNodes();
-        createNodes('grid');
-    } else if (event.key === 'r') {
-        clearNodes();
-        createNodes('radial');
+        intersectedNode.material.color.set(0xff0000);  // Change color on click
     }
 });
